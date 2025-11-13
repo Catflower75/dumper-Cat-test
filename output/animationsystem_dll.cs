67631a21fc7434e1c562ffaaa17905e9e40e4f9e
CatFlower75 <130034388+Catflower75@users.noreply.github.com>
2025-11-13 09:03:28 +0100

    Add files via upload

--- output/animationsystem_dll.cs
+++ output/animationsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-11-13 06:35:34.287773100 UTC
+// 2025-11-13 08:00:44.193961500 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: animationsystem.dll
